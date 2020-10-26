e1a63d7421630ed16edc2ce14494136324884b16
Piravin <piravin.kcm@gmail.com>
2020-10-26 15:13:12 +0530

    added header and footer to all routes

--- smart-site/src/components/signin.js
+++ smart-site/src/components/signin.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import { Footer } from './footer';
+import Header from './header';
+
 export function SignIn () {
     return(
+        <React.Fragment>
+        <Header />
         <div class="signin">
             <div>Sign In</div>
         </div>
+        <Footer />
+        </React.Fragment>
     )
 }
