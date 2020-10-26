92a91e67b4c4eebb747b3e862e5cce1b9040026e
ShakthiGanesh <shakthi012000@gmail.com>
2020-10-26 18:42:12 +0530

    Complete Sign-In page

--- smart-site/src/components/signin.js
+++ smart-site/src/components/signin.js
@@ -1,7 +1,23 @@
+import React from 'react'
+import '../css/signin.css'
+import { Button, Form } from 'react-bootstrap'
 export function SignIn () {
     return(
         <div class="signin">
-            <div>Sign In</div>
+            <div id='signin-header'>Sign In</div>
+            <Form>
+                <Form.Group controlId="formGroupEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" required/>
+                </Form.Group>
+                <Form.Group controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" required/>
+                </Form.Group>
+                <Button variant="custom" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
